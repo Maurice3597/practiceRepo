8bb45dae38b4614d02f4110d678e9f5e47ccfcee
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-04 11:10:18 +0100

    Added a comment to index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#This code produces the multiplication table for any chosen number
 N = int(input("Enter the number whose multiplication table you're looking for: "))
 print(f"\nThe multiplcation table for {N} is\n")
 for i in range(1, 13):
