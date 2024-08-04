3f6e4626ee6f1c346c7a4113eeb8e0664d7ca51e
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-04 09:49:08 +0100

    Modified index.py file

---
+++ index.py
@@ -0,0 +1,5 @@
+N = int(input("Enter the number whose multiplication table you're looking for: "))
+print(f"\nThe multiplcation table for {N} is\n")
+for i in range(1, 13):
+    print(f"{N} x {i} = {N*i}")
+print("Thank you")
