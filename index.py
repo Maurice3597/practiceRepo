30fa4b9e3cd8c5a82a0e2282f35046f042101a0b
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-04 11:26:46 +0100

    Added some new lines the index.py

--- index.py
+++ index.py
@@ -3,5 +3,11 @@ N = int(input("Enter the number whose multiplication table you're looking for: "
 print(f"\nThe multiplcation table for {N} is\n")
 for i in range(1, 13):
     print(f"{N} x {i} = {N*i}")
-print("Thank you")
-print("Hello World")
+print("Thank you\n")
+print("Hello World\n")
+
+multiples =[]
+for i in range(1,101):
+    if i % 3 == 0:
+        multiples.append(i)
+print(F" {multiples} is a list of multiple of 3")
