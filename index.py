71442aa355ea501070f554b3c4ec42e9b8cec27f
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-18 21:09:08 +0100

    Added the case match in the index.py file

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-# This code is for a todo app
-user_prompt = input("Enter an an option below to continue")
+# This code is for a todo Program
+
 print("""HINT
 	Add --> To add a task
 	Remove or Delete --> To delete a todo task
@@ -8,6 +8,32 @@ print("""HINT
 	Quit or Exit --> To quit or edit the todo
 	Completed --> To mark a todo task as completed""")
 
+user_prompt = input("Enter an an option below to continue")
+
 while True:
 	todo = input("Enter a a task to do")
-	match user_prompt
+	match user_prompt:
+		case "add":
+			todo = input("Add a TODO task")
+			file = ('todos.txt' , 'w')
+			todos = file.write(todo)
+			file.close()
+		
+		case "edit":
+
+		case "remove" | "Delete":
+			todo = input("Enter the TODO task you want to delete or remove: ")
+			print(f"{todo} removed from list")
+
+		case "show" | "display":
+
+		case "complete" | "done":
+			todo = input("Enter the TODO task you have completed: ")
+			todos = todos.remove(todo)
+			print(f"{todo} completed and removed from list")
+		case "quit" | "exit":
+			break
+
+		case _:
+			print("Sorry, you typed the wrong command\nSee the hint for the right commands")
+
