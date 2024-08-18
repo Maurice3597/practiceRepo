008af81eb14b2b0b96ac61e4f3643d82c7c5bf39
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-18 23:49:26 +0100

    Updated code in file index.py

--- index.py
+++ index.py
@@ -1,31 +1,36 @@
 # This code is for a todo Program
 
-print("""HINT
+print("""		HINT
 	Add --> To add a task
 	Remove or Delete --> To delete a todo task
 	Edit --> To edit the todo list
 	Show or Display --> To show or display todo list
 	Quit or Exit --> To quit or edit the todo
-	Completed --> To mark a todo task as completed""")
+	Completed --> To mark a todo task as completed\n""")
 
-user_prompt = input("Enter an an option below to continue")
-
+todos =[]
 while True:
-	todo = input("Enter a a task to do")
-	match user_prompt:
+	user_action = (input("Enter an option here to continue: ")).lower()#+ "\n"
+	match user_action:
 		case "add":
-			todo = input("Add a TODO task")
-			file = ('todos.txt' , 'w')
-			todos = file.write(todo)
+			todo = input("Add a TODO task: ") + "\t"
+			todos.append(todo)
+			file = open('todos.txt', 'w')
+			file.writelines(todos)
 			file.close()
+			for i, j in enumerate(todos):
+				row = f"{i+1}-{j}"
+				print(row)
 		
 		case "edit":
+			""
 
 		case "remove" | "Delete":
 			todo = input("Enter the TODO task you want to delete or remove: ")
 			print(f"{todo} removed from list")
 
 		case "show" | "display":
+			""
 
 		case "complete" | "done":
 			todo = input("Enter the TODO task you have completed: ")
