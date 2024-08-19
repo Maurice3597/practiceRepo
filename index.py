ede4d877a163f12a4a00a555b2882a6ee8f8416d
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-19 10:30:00 +0100

    Cleaned the previous code for multiplication table

--- index.py
+++ index.py
@@ -43,15 +43,3 @@ while True:
 			print("Sorry, you typed the wrong command\nSee the hint for the right commands")
 
 
-#This code produces the multiplication table for any chosen number
-n = int(input("Enter the number whose multiplication table you're looking for: "))
-print(f"\nThe multiplcation table for {n} is\n")
-for i in range(1, 13):
-    print(f"{n} x {i} = {n * i} \n")
-print("Hello World\n")
-
-multiples =[]
-for k in range(1,101):
-    if k % 3 == 0:
-        multiples.append(k)
-print(F" {multiples} is a list of multiple of 3")
