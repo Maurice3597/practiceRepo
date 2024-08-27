a154291822a79cb0d623a51184ee1f1661757d8f
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-08-27 05:17:36 +0100

    Modoified the EDIT, COMPLETED and DELETE options

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ while True:
         todos.append(todo)
         with open('todos.txt','w') as file:
             file.writelines(todos)
-        print(f"{todo}Successfully added to the the TODO list")
+        print(f"\"{todo}\" Successfully added to the the TODO list")
         
     elif user_prompt.startswith("edit"):
         New_todo = (user_prompt[4:]).lstrip() + "\n"
@@ -28,24 +28,86 @@ while True:
         with open('todos.txt', 'r') as file:
                 todos = file.readlines()
 
-        print(f"Are you sure you want to edit {todos[To_edit]}??\n")
+        print(f"Are you sure you want to edit \"{todos[To_edit]}\" ??\n")
         Response = (input("Enter Yes or No to proceed: ")).capitalize()
 
         if Response == "Yes":
             with open('todos.txt', 'r') as file:
                 todos = file.readlines()
 
-            todos[To_edit] = New_todo
+            todos[To_edit - 1] = New_todo
             
             with open('todos.txt', 'w') as file:
                 todos = file.writelines(todos)
-                print(f"{todos[To_edit]} succesfully edited to {New_todo}")
+                #print(f"{todos[To_edit]} succesfully edited to {New_todo}")
         
         elif Response == "No":
             exit
 
         else:
-            print("Wrong command")
+            print("Wrong command\nPlease enter 'YES' to edit or 'NO' to cancel editing")
+
+    elif user_prompt.startswith("show"):
+        with open('todos.txt') as file:
+            todos = file.readlines()
+
+        todos = [item.strip('\n') for item in todos]
+        for position, task in enumerate(todos):
+            print(f"{position + 1}- {task}")
+        
+    elif user_prompt.startswith("delete"):
+        To_delete = int(input("Enter the todo number you want to delete: "))
+
+        with open('todos.txt') as file:
+            todos = file.readlines()
+
+        print(f"Are you sure you want to delete \"{todos[To_delete - 1]}\" ??\n:")
+
+        Response = (input("Enter Yes or No to proceed: ")).capitalize()
+
+        if Response == "Yes":
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todos.remove(todos[To_delete - 1])
+            
+            with open('todos.txt', 'w') as file:
+                todos = file.writelines(todos)
+                #print(f"{todos[To_edit]} succesfully edited to {New_todo}")
+        
+        elif Response == "No":
+            exit
+            
+        else:
+            print("Wrong command\nPlease enter 'YES' to delete or 'NO' to cancel deleting")
+
+    elif user_prompt.startswith("completed"):
+        Completed = int(input("Enter the todo number you want to mark as completed: "))
+
+        with open('todos.txt') as file:
+            todos = file.readlines()
+
+        print(f"Are you sure you want to mark \"{todos[Completed - 1]}\" as completed??\n"
+              "Note that this action will also delete the completed task from the list:")
+
+        Response = (input("Enter Yes or No to proceed: ")).capitalize()
+
+        if Response == "Yes":
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todos.remove(todos[Completed - 1])
+            
+            with open('todos.txt', 'w') as file:
+                todos = file.writelines(todos)
+            print(f"\"{todos[Completed - 1]}\" completed successfully and removed from the list")
+        
+        elif Response == "No":
+            exit
+            
+        else:
+            print("Wrong command\nPlease enter 'YES' to approve task as completed or 'NO' to cancel")
+
 
     else:
         break
--- trial.py
+++ trial.py
@@ -1,4 +1,19 @@
 
-with open('todos.txt', 'r') as file:
-    todos = file.readlines()
-print(todos)
+To_delete = int(input("Enter the todo number yu want to delete: "))
+Response = (input("Enter Yes or No to proceed: ")).capitalize()
+
+if Response == "Yes":
+    with open('todos.txt', 'r') as file:
+        todos = file.readlines()
+
+    todos.remove(todos[To_delete - 1])
+            
+    with open('todos.txt', 'w') as file:
+        todos = file.writelines(todos)
+        #print(f"{todos[To_edit]} succesfully edited to {New_todo}")
+        
+elif Response == "No":
+    exit
+
+else:
+    print("Wrong command\nPlease enter 'YES' to delete or 'NO' to cancel deleting")
