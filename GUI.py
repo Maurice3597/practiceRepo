d715b16e6784d58355e279ce81d5339818f9f3da
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-09-02 04:37:31 +0100

    Created basic GUI without interaction

---
+++ GUI.py
@@ -0,0 +1,9 @@
+import FreeSimpleGUI as fsg
+
+label = fsg.Text("Type in a to-do")
+input_box = fsg.InputText(tooltip="Enter  todo")
+add_button =fsg.Button("Add")
+windows = fsg.Window("Your Favourite TODO App", layout=[[label], [input_box, add_button]])
+windows.read()
+print("Hello")
+windows.close()
