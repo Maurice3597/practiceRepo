c4d3bb93fe68ac865140ff19322411b1f2043d67
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-09-05 08:25:48 +0100

    Added the time and date feature and error display for adding empty todo

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 import FreeSimpleGUI as fsg
 import functions as fxns
+import time
 
+clock = fsg.Text("", key="clock")
 label = fsg.Text("Type in a to-do")
 input_box = fsg.InputText(tooltip="Enter  todo", key= "todo",
                           size=([35, 5]))
 
-add_btn = fsg.Button("Add")
+add_btn = fsg.Button("Add", size= 5)
 list_box = fsg.Listbox( values=fxns.get_todos(),
                         key= 'todos',
                         enable_events=True,
-                        size=[35, 15])
+                        size=[35, 10])
 
-edit_btn = fsg.Button("Edit")
+edit_btn = fsg.Button("Edit", size= 4)
 complete_btn = fsg.Button("Complete")
+clear_btn = fsg.Button("Clear", size= 5)
+exit_btn = fsg.Button("Exit", size= 4)
 
 windows = fsg.Window("Your Favourite TODO App",
-                      layout=[[label],
+                      layout=[[clock],[label],
                               [input_box, add_btn],
-                               [list_box, [edit_btn, complete_btn]]],
+                               [list_box, edit_btn, complete_btn]],
                         font= ('Helvetica', 15))
 
 while True:
-  event, value = windows.read()
-  print(f"event ={event} \nvalue = {value}")
+  event, value = windows.read(timeout=500)
+  windows['clock'].update(value= time.strftime("Date: %d-%b-%y  %I:%M:%S %p"))
 
   match event:
 
     case "Add":
       todos = fxns.get_todos()
-      new_todos = value['todo'] + "\n"
-      todos.append(new_todos)
-      fxns.write_todos(todos)
-      windows['todos'].update(values=todos)
+      new_todo = value['todo'] + "\n"
+
+      if new_todo =='' + "\n":
+          fsg.popup("Please Enter a TODO first",
+                    title="Error Message",
+                    font= ('Helvetica', 15))
+      else:
+          todos.append(new_todo)
+          fxns.write_todos(todos)
+          windows['todos'].update(values=todos)
+          windows['todo'].update(value="")
+
     case fsg.WIN_CLOSED:
       break
 
@@ -48,6 +60,7 @@ while True:
           todos[index] = new_todo
           fxns.write_todos(todos)
           windows['todos'].update(values=todos)
+          windows['todo'].update(value="")
         except IndexError:
             fsg.popup("No todo Selected‼️\nSelect a todo and try again",
                       title="ERROR 🚫⛔",
