af8679802d44a853f179e2685cfc13c14ecd6dc0
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-09-07 04:39:11 +0100

    fixed a bug in functions

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
-def get_todos(filepath):
+FILEPATH= 'todos.txt'
+
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as local_file:
         local_todos = local_file.readlines()
     return local_todos
 
 
-def write_todos(todo_arg, filepath):
+def write_todos(todo_arg, filepath=FILEPATH):
     with open(filepath, 'w') as local_file:
         local_file.writelines(todo_arg)
